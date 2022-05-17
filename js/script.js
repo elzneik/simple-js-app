48040adf6323633c37ddaff92bea101bf9cf375c
elzneik <104824463+elzneik@users.noreply.github.com>
2022-05-17 08:13:54 +0200

    Created PokemonList array

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let pokemonList [
+    {name: `Bulbasur`, height: `7`, types: [`grass`, `poison`, `flying`, `electric`]},
+    {name: `Jigglypuff`, height: `5`, types: [`steel`, `poison`]},
+    {name: `Pikachu`, height: `4`, types: [`ground`, `flying`, `steel`]},
+    {name: `Victreebel`, height: `17`, types: [`psychic`, `fire`, `ice`, `fairy`]},
+    {name: `Starmie`, height: `11`, types: [`dragon`, `ghost`, `bug`, `fire`, `ice`]}
+];
