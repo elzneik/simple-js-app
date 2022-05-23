7033d9b803dd367384e32f5f795598f9d3fcc114
elzneik <104824463+elzneik@users.noreply.github.com>
2022-05-23 09:46:33 +0200

    Change measurement number from string to number

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 let pokemonList = [
-    {name: `Bulbasur`, height: `0.7`, types: [`grass`, `poison`, `flying`, `electric`]},
-    {name: `Jigglypuff`, height: `0.5`, types: [`steel`, `poison`]},
-    {name: `Pikachu`, height: `0.4`, types: [`ground`, `flying`, `steel`]},
-    {name: `Victreebel`, height: `1.7`, types: [`psychic`, `fire`, `ice`, `fairy`]},
-    {name: `Starmie`, height: `1.1`, types: [`dragon`, `ghost`, `bug`, `fire`, `ice`]}
+    {name: `Bulbasur`, height: 0.7, types: [`grass`, `poison`, `flying`, `electric`]},
+    {name: `Jigglypuff`, height: 0.5, types: [`steel`, `poison`]},
+    {name: `Pikachu`, height: 0.4, types: [`ground`, `flying`, `steel`]},
+    {name: `Victreebel`, height: 0.7, types: [`psychic`, `fire`, `ice`, `fairy`]},
+    {name: `Starmie`, height: 1.1, types: [`dragon`, `ghost`, `bug`, `fire`, `ice`]}
 ];
 
 // Always add [i] behind the object variable.
