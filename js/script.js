63176f7040d2e20f9c71391ffb40ec5c46cc12c3
elzneik <104824463+elzneik@users.noreply.github.com>
2022-05-23 09:49:33 +0200

    Change Balbasur height from 0.7 to 1.7

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let pokemonList = [
-    {name: `Bulbasur`, height: 0.7, types: [`grass`, `poison`, `flying`, `electric`]},
+    {name: `Bulbasur`, height: 1.7, types: [`grass`, `poison`, `flying`, `electric`]},
     {name: `Jigglypuff`, height: 0.5, types: [`steel`, `poison`]},
     {name: `Pikachu`, height: 0.4, types: [`ground`, `flying`, `steel`]},
     {name: `Victreebel`, height: 0.7, types: [`psychic`, `fire`, `ice`, `fairy`]},
