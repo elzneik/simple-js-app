165c950129483349ccd618e7d9c9a26a6f9b932c
elzneik <104824463+elzneik@users.noreply.github.com>
2022-06-02 11:41:18 +0200

    wrap array pokemonList into an IIFE

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
+(function(){
 let pokemonList = [
     {name: "Bulbasur", height: 1.7, types: ["grass", "poison", "flying", "electric"]},
     {name: "Jigglypuff", height: 0.5, types: ["steel", "poison"]},
     {name: "Pikachu", height: 0.4, types: ["ground", "flying", "steel"]},
     {name: "Victreebel", height: 0.7, types: ["psychic", "fire", "ice", "fairy"]},
     {name: "Starmie", height: 1.1, types: ["dragon", "ghost", "bug", "fire", "ice"]}
-];
+]})();
 
-// Always add [i] behind the object variable.
-// Be aware of the ; - where to add them and wehre not
-// Set the loop parantheses to the end when adding conditinal sentences to a loop.
-pokemonList.forEach(function (pokemon){
+// adapt/conect always the function parameter and not the array name
+pokemonList.forEach(function(pokemon){
     document.write(pokemon.name +" "+"height is "+ pokemon.height +" m!"); 
 
     if(pokemon.height >= 1.7) {
