2ae9223179b9cc6d5bdd68407b39302b2e38d33a
elzneik <104824463+elzneik@users.noreply.github.com>
2022-06-03 07:10:18 +0200

    add parameter typeof into the function add to check if the type of data belong to an object

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+/*
+// Bonus Task: Filter
+const filterList = pokemonRepository;
+const result = filterList.filter(pokemonList.name);
+console.log(result);
+*/
+
+// wrap an IIEF methodology into a variable
+// the function expression holds an array, add function and getAll function
+// add function adds up information, so no return assigned 
+// add function used to specify data types added up parameter typeof was assigned 
 let pokemonRepository = (function(){
     let pokemonList = [
         {name: "Bulbasur", height: 1.7, types: ["grass", "poison", "flying", "electric"]},
@@ -5,10 +16,21 @@ let pokemonRepository = (function(){
         {name: "Pikachu", height: 0.4, types: ["ground", "flying", "steel"]},
         {name: "Victreebel", height: 0.7, types: ["psychic", "fire", "ice", "fairy"]},
         {name: "Starmie", height: 1.1, types: ["dragon", "ghost", "bug", "fire", "ice"]}
-    ];
-
+        ];
+        
+// Add function led add all data types. Precent it with typeof parameter and conditionals
     function add(pokemon){
-        pokemonList(pokemon);
+        if  (
+            typeof pokemon === "object" && 
+            "name" in pokemon &&
+            "height" in pokemon &&
+            "types" in pokemon
+            ){
+            pokemonList(pokemon);
+            }
+        else{
+            console.log("pokemon not found");
+            }
     }
     function getAll(){
         return pokemonList;
@@ -21,14 +43,24 @@ let pokemonRepository = (function(){
 
 // adapt/conect always the function parameter and not the array name
 pokemonRepository.getAll().forEach(function(pokemon){
-    document.write(pokemon.name +" "+"height is "+ pokemon.height +" m!"); 
-
+    //document.write(pokemon.name +" "+"height is "+ pokemon.height +" m!"); 
+/*
     if(pokemon.height >= 1.7) {
-        document.write("     - I\`m size L. Wow, that\`s big!"+ "<br>");
+        //document.write("     - I\`m size L. Wow, that\`s big!"+ "<br>");
     } else if(pokemon.height >=0.5 && pokemon.height <=1.1) {
-        document.write("     - I\`m size M. "+ "<br>");
+        //document.write("     - I\`m size M. "+ "<br>");
     } else {
-        document.write("     - I\`m size S. " + "<br>");
+        //document.write("     - I\`m size S. " + "<br>");
     }
+*/
+
+    let DomManipulatingPractise = document.querySelectorAll(".pokemon-list");
+
+    let listItem = document.createElement("li");
+    let button = document.createElement ("button");
+    button.innerText = " ";
+    //listItem.appendChild(button);
+
+
 });
 
