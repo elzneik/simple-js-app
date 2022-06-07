444b4132ba04e7dc0bcea8a251392c7759a6236a
elzneik <104824463+elzneik@users.noreply.github.com>
2022-06-07 15:42:39 +0200

    delete objects in the pokemon-list array and leave array empfty

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
 let pokemonRepository = (function(){
-    let pokemonList = [
-        {name: "Bulbasur", height: 1.7, types: ["grass", "poison", "flying", "electric"]},
-        {name: "Jigglypuff", height: 0.5, types: ["steel", "poison"]},
-        {name: "Pikachu", height: 0.4, types: ["ground", "flying", "steel"]},
-        {name: "Victreebel", height: 0.7, types: ["psychic", "fire", "ice", "fairy"]},
-        {name: "Starmie", height: 1.1, types: ["dragon", "ghost", "bug", "fire", "ice"]}
-    ];
+    let pokemonList = [ ];
+    
     function add(pokemon){
         pokemonList.add(pokemon);
     }
