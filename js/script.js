c457413552a8597fb388b10874ef13239cfce029
elzneik <104824463+elzneik@users.noreply.github.com>
2022-06-07 18:22:28 +0200

    correct code syntax

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let pokemonRepository = (function(){
     let pokemonList = [];
-    let apiURL ="https://pokeapi.co/api/v2/pokemon/?limit=20";
+    let apiUrl ="https://pokeapi.co/api/v2/pokemon/?limit=20";
 
     function add(pokemon){
         if(
@@ -43,15 +43,16 @@ let pokemonRepository = (function(){
     }
 
     function loadList(){
-        return fetch(apiURL).then(function(response){
+        return fetch(apiUrl).then(function(response){
             return response.json();
         }).then(function(json){
             json.result.forEach(function(item){
                 let pokemon = {
                     name: item.name,
-                    detailsURL: item.url
+                    detailsUrl: item.url
                 };
                 add(pokemon);
+                console.log(pokemon);
             });
         }).catch(function(e){
             console.error(e);
