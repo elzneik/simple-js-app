936057437fe932400d4def6238bbc70aa4f1114c
elzneik <104824463+elzneik@users.noreply.github.com>
2022-06-27 10:29:35 +0200

    add ESlint file to the root folder - rule: allow only single quotes

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+{
+    "env": {
+        "es6": true,
+        "browser": true
+    },
+    "extends": [
+        "eslint:recommended"
+    ],
+    "rules": {
+        "quotes": ["error", "single"]
+    }
+}
